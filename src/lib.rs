ffb8b30fc5f54cfb2a559057be0ce6eaff82ab41
FanJiang <fanjiang233@proton.me>
2024-04-13 17:20:54 +0800

    lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,24 @@ pub mod basic_type;
 pub mod box_module;
 pub mod const_static;
 pub mod enum_pattern_match;
+pub mod fn_move_borrow_mut;
+pub mod fn_return_value_ref;
 pub mod function;
+pub mod higher_func;
 pub mod if_match;
 pub mod loops;
 pub mod mem_manage;
 pub mod owner_ship;
+pub mod result_option_panic;
 pub mod str_string;
 pub mod struct_module;
 pub mod tuple_array;
 pub mod utils;
-pub mod fn_move_borrow_mut;
-pub mod fn_return_value_ref;
-pub mod higher_func;
-pub mod result_option_panic;
 
 pub mod unwrap;
 
 pub mod custom_error;
 
 pub mod borrow_checker;
+
+pub mod lifetimes;
---
+++ src/lifetimes/mod.rs
@@ -0,0 +1,37 @@
+#[test]
+fn lifetimes_test() {
+    fn longest_str<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+        if s1.len() >= s2.len() {
+            s1
+        } else {
+            s2
+        }
+    }
+
+	// 这个函数更通用
+    fn longest_str_pro<'a, 'b, 'out>(s1: &'a str, s2: &'b str) -> &'out str
+    where
+		// 'out 的生命周期是 'a 和 'b 的交集
+        'a: 'out, // 'a 的生命周期 >= 'out 的生命周期
+        'b: 'out, // 'b 的生命周期 >= 'out 的生命周期
+    {
+        if s1.len() >= s2.len() {
+            s1
+        } else {
+            s2
+        }
+    }
+
+    assert_eq!(longest_str("hello", "hello_world"), "hello_world");
+
+	let s1 = "hello";
+	{
+		let mut s2 = "s2222222222";
+		assert_eq!(longest_str(s1, s2), s2);
+
+		s2 = "s2";
+		assert_eq!(longest_str_pro(s1, s2), s1);
+	}
+
+
+}
